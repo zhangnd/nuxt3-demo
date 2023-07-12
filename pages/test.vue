4448248ddea0cc1884c659c37d1626512789b768
zhangnd <920563959@qq.com>
2023-07-12 12:14:39 +0800

    Initial commit

---
+++ pages/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div>{{ message }}</div>
+    <div>{{ list.length }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $api } = useNuxtApp()
+const message = ref('')
+const list: Ref<Array<any>> = ref([])
+const getSession = () => {
+  $api.getSession()
+    .then(res => {
+
+    })
+    .catch(err => {
+      console.error(err)
+      message.value = 'hello world'
+      list.value = [{}, {}]
+    })
+}
+if (process.server) {
+  getSession()
+}
+// getSession()
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
