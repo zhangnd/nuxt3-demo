02f355c1b3d75239b2cb6a36fb6800a23ad4f95e
zhangnd <920563959@qq.com>
2023-07-12 12:38:37 +0800

    Initial commit

--- apis/user.ts
+++ apis/user.ts
@@ -1,5 +1,21 @@
 import http from '@/utils/http'
 
+export function register(body: any) {
+  return http.post('/user/register', body)
+}
+
 export function login(body: any) {
   return http.post('/user/login', body)
+}
+
+export function logout() {
+  return http.post('/user/logout')
+}
+
+export function getSession() {
+  return http.get('/user/getSession')
+}
+
+export function getUser(params: any) {
+  return http.get('/user/getUser', params)
 }
